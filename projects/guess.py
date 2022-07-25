a344dd7a691d3a2749b45b27436fd8e29374b467
Vina Chen <cvina2021@gmail.com>
2022-07-25 15:44:05 -0400

    working on it

--- projects/guess.py
+++ projects/guess.py
@@ -1,1 +1,14 @@
 #your code here
+from numpy import random
+again = "Yes";
+
+while(again == "Yes"):
+    rm = random.randint(100)
+    user = int(input("Enter your guess"))
+    if user < rm:
+        print("Too Low")
+    elif user > rm:
+        print("Too High")
+    else:
+        print("Congratulations")
+    again = input("Do you want to play again?")
