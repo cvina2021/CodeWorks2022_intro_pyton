55efd8e002edd18a208df0a7927b1cc4b5ea0768
Vina Chen <cvina2021@gmail.com>
2022-07-27 13:35:30 -0400

    working on it

--- projects/expensereport.py
+++ projects/expensereport.py
@@ -2,10 +2,19 @@
 from matplotlib import pyplot as plt
 
 #create empty lists
-
+month = []
+income = []
+expense = []
+profit = []
 
 #open the file
+file = open("expense_report.csv", "r")
 
 #loop through the file line by line and populate the lists apropriately
-
+for x in file:
+    line[] = x.readline().split(",")
+    month.add(line[0])
+    income.add(line[1])
+    expense.add(line[2])
+    profit.add(line[1] - line[2])
 #create the plot
